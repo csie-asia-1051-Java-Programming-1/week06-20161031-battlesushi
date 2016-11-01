f23474d0c1aabd1cc89ea35b0de6d881dedb3c66
battlesushi <105021030@live.asia.edu.tw>
2016-11-01 17:13:37 +0800

    hw

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,24 @@
 package hw;
-
+import java.util.*;
 public class hw01 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		System.out.print(	fun(sc.nextFloat(),sc.nextShort())	);
 	}
+	public static float fun(float v1,short type) {
+		float sum=0,C=0,F=0;
+		if(type==1){
+			C=(v1-32)*5/9;
+			sum=C;
+		}
+		else if(type==2){
+			F=(v1*9/5)+32;
+			sum=F;
+		}
+		else
+			System.out.print("¿é¤J¿ù»~!!");
+		return sum;
+	}
+	
 
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,25 @@
 package hw;
-
+import java.util.*;
 public class hw02 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		System.out.print("輸入男(1)/女(2), 身高:");
+		System.out.print(fun(sc.nextShort(),sc.nextFloat()));
 	}
+	public static float fun(short x, float y) {
+		float out=0,b=0,g=0;
+		if(x==1){
+			b=(y-170)*0.6f+62;
+			out=b;
+		}
+		else if(x==2){
+			g=(y-158)*0.5f+52;
+			out=g;
+		}
+		else
+			System.out.print("輸入錯誤!!");
 
+		return out;
+	}
+	
 }
